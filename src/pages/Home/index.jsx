b06e8b4a9600d7c2acb18ed68e7e768a0ec856ee
Chairuabda <chairuabda@gmail.com>
2023-10-16 22:37:03 +0700

    Update Home
    Update Carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,12 @@
-import { Box } from "@chakra-ui/react"
-import { Navbar } from "../Components/Navbar"
+import { Box } from "@chakra-ui/react";
+import { Navbar } from "../Components/Navbar";
+import Carousel from "./Components/Carousel";
 
 export const Home = () => {
-    return (
-        <Box bgColor="home.primary" w={"100vw"}overflowX={"hidden"}>
-            <Navbar/>
-        </Box>
-    )
-}
+	return (
+		<Box bgColor="home.primary" w={"100vw"} overflowX={"hidden"}>
+			<Navbar />
+			<Carousel />
+		</Box>
+	);
+};
